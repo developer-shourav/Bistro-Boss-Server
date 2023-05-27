068cef3648ad3473345cac44bf4e38cccf4da42b
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-27 14:33:15 +0600

    database added and all menu get complete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+require('dotenv').config()
 const port = process.env.PORT || 7000 ;
 
 
@@ -10,6 +12,47 @@ const port = process.env.PORT || 7000 ;
 
 app.use(cors())
 app.use(express.json())
+
+
+
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.evuna6q.mongodb.net/?retryWrites=true&w=majority`;
+
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+    });
+
+
+    const menuCollection = client.db("bistroDB").collection("menu");
+
+    async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        app.get('/menu', async(req, res) => {
+           
+            const result = await menuCollection.find().toArray() ;
+            res.send(result)
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        /* await client.close(); */
+    }
+    }
+    run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to Bistro Boss')
