3712a7717722d18fcf4f7bd4603656d7d9a05e54
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-31 22:44:38 +0600

    vercel configaration added

--- index.js
+++ index.js
@@ -10,13 +10,7 @@ const port = process.env.PORT || 7000 ;
 
 // Middleware
 
-const corsConfig = {
-    origin: '',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
+app.use(cors())
 app.use(express.json())
 
 
