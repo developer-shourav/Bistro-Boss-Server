ea00930cd81bb2d303e7d6d4bf608313f652fe24
developer-shourav <mrinmoyraj823@gmail.com>
2023-06-05 07:11:23 +0600

    server side token generate complete

--- index.js
+++ index.js
@@ -11,8 +11,38 @@ const port = process.env.PORT || 7000 ;
 
 // Middleware
 
-app.use(cors())
-app.use(express.json())
+    app.use(cors());
+    app.use(express.json());
+
+    const verifyJWT = (req, res, next) => {
+        
+        const authorization = req.headers.authorization;
+
+        if(!authorization){
+           return res.status(401).send({error: true, message: 'unauthorized access'});
+        }
+
+    /*  
+        Remember the token formate: bearer token (example: bearer fldjfljdfljfflwoufy)
+        *But we need only the token code. 
+        *So we have to split the token with an empty space and have to take second indexed value;
+     */
+        const token = authorization.split(' ')[1];
+
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if(err){
+                return res.status(401).send({error:true, message:'unauthorized access'})
+            }
+
+            req.decoded = decoded;
+            next();
+        })
+
+
+    }
+
+
+
 
 
 
@@ -101,17 +131,23 @@ app.use(express.json())
         });
 
         /* -----------Shopping Cart  Related API------------ */
-        app.get('/carts', async(req, res) => {
+        app.get('/carts', verifyJWT,  async(req, res) => {
             const email = req.query.email ;
+
             if(!email){
                 res.send([])
             }
-            else{
 
-                const query = {email: email};
-                const result = await cartCollection.find(query).toArray();
-                res.send(result)
+            const decodedEmail = req.decoded.email;
+
+            if(email !== decodedEmail){
+                return res.status(403).send({error:true, message:'Forbidden access'})
             }
+
+
+            const query = {email: email};
+            const result = await cartCollection.find(query).toArray();
+            res.send(result)
         });
 
         app.post('/carts', async(req, res) => {
